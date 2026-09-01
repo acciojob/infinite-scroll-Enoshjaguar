d1c1bde765f021b087937c726ac5db78ca148f9d
Enoshjaguar <dasarienosh013@gmail.com>
2026-09-01 16:50:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+let ol = document.getElementById('infi-list')
+let start = 1
+let end = 10
+function add(start,end){
+for(let i=start;i<=end;i++){
+	let li = document.createElement('li')
+	li.textContent = `Item ${i}`
+	ol.appendChild(li)
+}
+}
+add(start,end)
+
+
+ol.addEventListener('scroll',()=>{
+	let totalheight = ol.scrollHeight
+	let clientHeight = ol.clientHeight
+	let scrolled = ol.scrollTop
+	if(scrolled+clientHeight>=totalheight){
+		start=end+1
+		end = end+2
+		add(start,end)
+	}
+})
 
